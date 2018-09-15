1de923e8996022c0daf2e96314e068ea6643cc1a
vchoisk <vchoisk@gmail.com>
2018-09-15 20:11:44 +0900

    added button

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -2,14 +2,22 @@ import * as React from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 import { withRouter } from 'react-router'
 
+import { Button } from '../widgets/Button'
+
 import menu from '../asset/icons/menu.svg'
 import menuSelected from '../asset/icons/menu-selected.svg'
 import cart from '../asset/icons/cart.svg'
 import cartSelected from '../asset/icons/cart-selected.svg'
 
 class HeaderWithoutRouter extends React.Component<RouteComponentProps, {}> {
+  public handleNavigateTo = (path: string) => {
+    this.props.history.push(path)
+  }
+
   public render() {
-    const pathToTextMapper = {
+    const { location } = this.props
+
+    const pathToNameMapper = {
       '/': '맥주담기',
       '/cart': '내 카트',
     }
@@ -18,22 +26,24 @@ class HeaderWithoutRouter extends React.Component<RouteComponentProps, {}> {
       <div className="header">
         <h1 className="header__title">Beer Shop</h1>
         <div className="header__nav-bar nav-bar">
-          <div className="nav-bar__current-location">
-            {pathToTextMapper[this.props.location.pathname]}
-          </div>
+          <h2 className="nav-bar__current-location">
+            {pathToNameMapper[location.pathname]}
+          </h2>
           <div className="nav-bar__icon-buttons">
-            <img
-              className="nav-bar__icon-button--shop"
-              src={this.props.location.pathname === '/' ? menuSelected : menu}
-              alt="go to shop"
-            />
-            <img
-              className="nav-bar__icon-button--cart"
-              src={
-                this.props.location.pathname === '/cart' ? cartSelected : cart
-              }
-              alt="go to cart"
-            />
+            <Button onClick={this.handleNavigateTo.bind(this, '/')}>
+              <img
+                className="nav-bar__icon-button--shop"
+                src={location.pathname === '/' ? menuSelected : menu}
+                alt="go to shop"
+              />
+            </Button>
+            <Button onClick={this.handleNavigateTo.bind(this, '/cart')}>
+              <img
+                className="nav-bar__icon-button--cart"
+                src={location.pathname === '/cart' ? cartSelected : cart}
+                alt="go to cart"
+              />
+            </Button>
           </div>
         </div>
       </div>
---
+++ src/widgets/Button.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react'
+import { isUndefined } from 'lodash'
+
+export interface IButtonProps {
+  children: JSX.Element
+  variant?: string
+  primary?: boolean | undefined
+  secondary?: boolean | undefined
+  disabled?: boolean | undefined
+  className?: string
+  onClick?: () => void
+}
+
+export const Button = (props: IButtonProps) => {
+  const addClassName = (include: boolean, className: string) => {
+    if (include) {
+      buttonClassNames.push(className)
+    }
+  }
+
+  const variantMapper = {
+    bordered: 'bordered',
+    contained: 'contained',
+  }
+
+  const buttonClassNames = ['widget-button']
+  addClassName(!isUndefined(props.className), props.className || '')
+  addClassName(!isUndefined(props.primary), 'primary')
+  addClassName(!isUndefined(props.secondary), 'secondary')
+  addClassName(!isUndefined(props.disabled), 'disabled')
+  addClassName(
+    !isUndefined(props.variant),
+    props.variant ? variantMapper[props.variant] : ''
+  )
+
+  return (
+    <div
+      className={buttonClassNames.join(' ')}
+      onClick={props.onClick || undefined}
+    >
+      {props.children}
+    </div>
+  )
+}
