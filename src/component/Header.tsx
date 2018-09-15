e47d3b7aa4a6441202fccd8533367e121edb552d
vchoisk <vchoisk@gmail.com>
2018-09-15 17:06:42 +0900

    import icon based on path / add cart icon

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -2,6 +2,11 @@ import * as React from 'react'
 import { RouteComponentProps } from 'react-router-dom'
 import { withRouter } from 'react-router'
 
+import menu from '../asset/icons/menu.svg'
+import menuSelected from '../asset/icons/menu-selected.svg'
+import cart from '../asset/icons/cart.svg'
+import cartSelected from '../asset/icons/cart-selected.svg'
+
 class HeaderWithoutRouter extends React.Component<RouteComponentProps, {}> {
   public render() {
     const pathToTextMapper = {
@@ -16,7 +21,20 @@ class HeaderWithoutRouter extends React.Component<RouteComponentProps, {}> {
           <div className="nav-bar__current-location">
             {pathToTextMapper[this.props.location.pathname]}
           </div>
-          <div className="nav-bar__icon-buttons">buttons</div>
+          <div className="nav-bar__icon-buttons">
+            <img
+              className="nav-bar__icon-button--shop"
+              src={this.props.location.pathname === '/' ? menuSelected : menu}
+              alt="go to shop"
+            />
+            <img
+              className="nav-bar__icon-button--cart"
+              src={
+                this.props.location.pathname === '/cart' ? cartSelected : cart
+              }
+              alt="go to cart"
+            />
+          </div>
         </div>
       </div>
     )
