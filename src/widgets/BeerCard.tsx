a53010d15f67ffe06696c5f116f2dc621466b110
vchoisk <vchoisk@gmail.com>
2018-09-16 12:10:47 +0900

    connect beer card with data

--- src/widgets/BeerCard.tsx
+++ src/widgets/BeerCard.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 
-import { Ibeer } from '../interface'
+import { Button } from '../widgets/Button'
+
+import { Ibeer, Itag } from '../interface'
+import beerImg from '../asset/beer_x1.png'
 
 export interface IBeerCardProps {
   beer: Ibeer
@@ -8,6 +11,45 @@ export interface IBeerCardProps {
 
 export class BeerCard extends React.Component<IBeerCardProps, {}> {
   public render() {
-    return <div>{this.props.beer.name}</div>
+    return (
+      <div className="beer-card">
+        <div className="beer-card__img-container">
+          <img src={beerImg} alt={this.props.beer.name} />
+        </div>
+
+        <div className="beer-card__information">
+          <div className="beer-card__name">{this.props.beer.name}</div>
+          <div className="beer-card__tags">
+            {this.props.beer.tags.map((tag: Itag) => (
+              <span className="beer-card__tag" key={tag.key}>
+                {tag.name}
+              </span>
+            ))}
+          </div>
+          <div className="beer-card__price">
+            {this.props.beer.price}
+            <span>원</span>
+          </div>
+          <div className="beer-card__counts">
+            <div className="beer-card__stock-count">
+              <span className="beer-card__stock-label">재고</span>
+              {this.props.beer.stock}
+            </div>
+            <div className="beer-card__card-count">
+              <span className="beer-card__count-label">수량</span>
+              {this.props.beer.name}
+            </div>
+          </div>
+        </div>
+        <div className="beer-card__buttons">
+          <Button secondary variant="contained">
+            <span>빼기</span>
+          </Button>
+          <Button primary variant="contained">
+            <span>담기</span>
+          </Button>
+        </div>
+      </div>
+    )
   }
 }
